14e8a0439225422c9143713e451aaa2c30557e7d
Gomez Angeles <gca935@gmail.com>
2021-11-23 18:00:45 -0300

    Agregue el slider y la funcionalidad en el formulario de inicio que al seleccionar una provincia te muestre el instituto de ese lugar

---
+++ js/funciones-index.js
@@ -0,0 +1,40 @@
+let opProvincias = {
+    bsas : ["", "Instituto Mataderos"],
+    cba : ["", "Instituto Milenio"],
+    rnegro : ["", "Instituto D.Alighieri"],
+    salta : ["", "Instituto Güemes"]
+}
+$("document").ready(function(){
+    $('.slider').slick({
+        autoplay:true,
+        /*autoplaySpeed: 5000,
+        speed: 300,*/
+        dots:true,
+        fade:true,
+        infinite: true,
+        mobileFirst: true
+     } ); 
+     
+    $("#provincia").change(function(){
+        mostrarCombo();
+        mostrarDatos();
+    });
+});
+
+function mostrarCombo(){
+    let seleccion =  $("#provincia").val();
+    $("#establecimiento").empty();
+
+    opProvincias[seleccion].forEach(function(element, index){
+        $("#establecimiento").append('<option value="'+element+'">'+element+'</option>');
+    });
+}
+
+function mostrarDatos(){
+    
+		let info = $('#provincia').options[$('#provincia').selectedIndex].text;
+
+		localStorage.setItem($("#provincia"), info);
+
+		$("#algo").innerHTML = localStorage.getItem($("#provincia"));
+}
