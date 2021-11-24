2a2a3d29d4f9cec0ac1a2e9a0dd349ddd87d788a
alvezlilian <lialvez@unlam.edu.ar>
2021-11-24 11:05:49 -0300

    agregue funcionamiento a los establecimientos

--- js/funciones-establecimientos.js
+++ js/funciones-establecimientos.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
- 
+  $("#opcion0").hide(); 
     $("#sede").change(function () {
  
         ocultarEstablecimiento($(this).val());
@@ -8,11 +8,23 @@ $(document).ready(function(){
 });
  
 function ocultarEstablecimiento(valor){
-switch(valor){
-    case 1:
-        $("#opcion-1").fadeOut();
-        break;
- 
-}
+    $("#opcion1").hide(); 
+    $("#opcion2").hide();
+    $("#opcion3").hide();
+     $("#opcion4").hide(); 
+     $("#opcion0").hide();
+     $("#opcion0").empty();
     
+    
+     
+     if(valor!=0){
+         $("#opcion"+valor+"").show();
+     }else{
+         $("#opcion0").append("<p>Filtra por provincia para encontrar tu sede mas cercana</p>");
+         $("#opcion0").show(); 
+     }
+        
+     
+        
+      
 }
